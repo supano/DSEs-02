17fd4ae68fdb996e7d17e8db2bf099d6f35349bd
supano <supano1995@gmail.com>
2020-08-23 14:19:45 +0700

    init

---
+++ api.py
@@ -0,0 +1,37 @@
+import flask
+import pickle
+import numpy as np
+from flask import request as fl_requests, jsonify
+from botnoi import cv
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>DES G6</p>"
+
+
+modFile = './models/mymodel.p'
+mymod = pickle.load(open(modFile,'rb'))
+def predictimg(imgurl):
+  a = cv.image(imgurl)
+  feat = a.getresnet50()
+  probList = mymod.predict_proba([feat])[0]
+  maxprobind = np.argmax(probList)
+  prob = probList[maxprobind]
+  outclass = mymod.classes_[maxprobind]
+  result = {}
+  result['class'] = outclass
+  result['probability'] = prob
+  return result
+
+@app.route('/api', methods=['GET'])
+def api():
+    if 'url' in fl_requests.args:
+        url = fl_requests.args['url']
+    else:
+        return "Error: No url field provided. Please specify an url."
+
+    return predictimg(url)
+
+app.run("localhost","80")
---
+++ readme.md
@@ -0,0 +1,6 @@
+### Installation
+pip install flask
+pip install botnoi
+pip install Pillow
+pip install -U scikit-learn scipy matplotlib
+pip install keras
